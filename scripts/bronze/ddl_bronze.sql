fc41933134710688db7621d081041e40374fda36
chack-attack <chackriya@gmail.com>
2025-08-27 11:08:52 -0400

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -32,7 +32,7 @@ CREATE TABLE bronze.crm_prd_info (
 	prd_cost	INT, 
 	prd_line	NVARCHAR(50),
 	prd_start_dt DATETIME, 
-	prd_end_	DATETIME
+	prd_end_dt	DATETIME
 );
 
 IF OBJECT_ID('bronze.crm_sales_details' , 'U') IS NOT NULL
