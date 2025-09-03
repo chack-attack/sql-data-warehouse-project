d3c0dd0c6b48b712d750ea303d8ed6517f8e1fb7
chack-attack <chackriya@gmail.com>
2025-09-03 13:40:00 -0400

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -20,7 +20,7 @@ Usage Example:
 
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
-	DECLARE @start_time DATETIME,@end_time DATETIME; @batch_start_time DATETIME, @batch_end_time DATETIME;
+	DECLARE @start_time DATETIME,@end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME;
 	BEGIN TRY
 		PRINT '================================================';
 		PRINT 'Loading Bronze Layer';
